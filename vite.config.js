58bcc5c6264a263d3afe9244754721e4a4507976
Joel Klein <joel.klein@fresenius-kabi.com>
2026-03-31 14:29:58 +0200

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PickMaster Twin - Fast Pattern Editor',
+        short_name: 'PM FPE',
+        description: 'Ein lokaler Editor für PickMaster Twin Container Patterns',
+        theme_color: '#2563eb',
+        background_color: '#f9fafb',
+        display: 'standalone',
+      }
+    })
   ],
 })
