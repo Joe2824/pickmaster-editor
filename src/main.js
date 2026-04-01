905f8f53db161a7b331dc4783478f2d568d68826
Joel Klein <joel.klein@fresenius-kabi.com>
2026-04-01 09:49:04 +0200

    update not working

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './style.css'
 import Alpine from 'alpinejs'
- 
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
 window.Alpine = Alpine
  
 Alpine.start()
